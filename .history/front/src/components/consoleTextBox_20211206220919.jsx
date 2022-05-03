e8fa7683317bf5fe08afc32299462140f1cd1507
birucan <birucandev@gmail.com>
2022-05-03 01:26:12 -0400

    history?

--- .history/front/src/components/consoleTextBox_20211206220919.jsx
+++
@@ -1,9 +0,0 @@
-import React,{useState} from "react";
-
-const ConsoleLine = ({dir}) => {
-    const [text, settext] = useState(initialState)
-    return (
-        <input/>
-    );
-}
-export default ConsoleLine;
