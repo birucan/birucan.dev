28810ac3e0fc04588274ce72b97738e02b284e9a
birucan <birucandev@gmail.com>
2022-05-05 00:10:48 -0400

    typo
    qa
    :qa
    q
    git commit -m typo

--- front/src/components/Applications/IntroWindow/IntroWindow.jsx
+++ front/src/components/Applications/IntroWindow/IntroWindow.jsx
@@ -8,7 +8,7 @@ export default function IntroWindow() {
     return (
 
                 <>
-                    <p style={{ textAlign: "center" }}>Hello I'm Tomas Kavanagh, this will be my portafolio site, it's meant to imitate a functional windows98 desktop, currently a work in progress, for now you can visit my github</p>
+                    <p style={{ textAlign: "center" }}>Hello I'm Tomas Kavanagh, this will be my portafolio site, it's meant to immitate a functional windows98 desktop, currently a work in progress, for now you can visit my github</p>
                     <br/><br/>
 
                     <div className="field-row" style={{ justifyContent: "center" }}>
